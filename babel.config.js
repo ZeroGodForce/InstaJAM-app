8e56b32311708075be66c9357bd57cc4bf47c343
Captain Buckman <ZeroGodForce@users.noreply.github.com>
2023-08-16 10:29:16 +0100

    [CONFIG] [BUGFIX] Fixed incorrect babel configuration for dotenv settings

--- babel.config.js
+++ babel.config.js
@@ -2,15 +2,17 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    ["module:react-native-dotenv", {
-      "envName": "APP_ENV",
-      "moduleName": "@env",
-      "path": ".env",
-      "blacklist": null,
-      "whitelist": null,
-      "safe": false,
-      "allowUndefined": true,
-      "verbose": false
-    }]
+    plugins: [
+      ['module:react-native-dotenv', {
+        'envName': 'APP_ENV',
+        'moduleName': '@env',
+        'path': '.env',
+        'blacklist': null,
+        'whitelist': null,
+        'safe': false,
+        'allowUndefined': true,
+        'verbose': false
+      }]
+    ]
   };
 };
