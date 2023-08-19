f2d180ae95ba967dacdd06f89627bbcae4d774bf
Captain Buckman <ZeroGodForce@users.noreply.github.com>
2023-08-19 14:52:39 +0100

    [IMAGE] Reworked layout

--- src/screens/ImageModal.tsx
+++ src/screens/ImageModal.tsx
@@ -1,8 +1,11 @@
-import React, { useLayoutEffect } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import React, { useLayoutEffect, useState } from 'react';
+import { ImageBackground, StyleSheet, Text, View, Dimensions, Pressable } from 'react-native';
 
 export const ImageModal = ({ navigation, route }) => {
     const { image } = route.params;
+    const imageWidth = Dimensions.get('window').width;
+    const imageHeight = (image.width && image.height) ? (image.height / image.width) * imageWidth : imageWidth;
+    const [showBlackout, setShowBlackout] = useState(true);
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -11,37 +14,48 @@ export const ImageModal = ({ navigation, route }) => {
         });
     }, [navigation]);
 
+    const toggleBlackout = () => {
+        setShowBlackout(!showBlackout);
+    };
+
     return (
-        <View>
-            <Text>{image.description}</Text>
-            <Text>{image.width} x {image.height}</Text>
-            <Text>Size: {image.filesize}</Text>
-            <Text>{image.createdAt}</Text>
-            <Image
+        <Pressable onPress={toggleBlackout} style={{ flex: 1 }}>
+            <ImageBackground
                 source={{ uri: image.imagePath }}
-                style={styles.photo}
+                style={[styles.photo, { height: imageHeight }]}
+                resizeMode="contain"
                 accessibilityIgnoresInvertColors
-            />
-        </View>
-    )
-}
+            >
+                {showBlackout && (
+                    <View style={styles.blackout}>
+                        <Text style={styles.text}>{image.description}</Text>
+                        <Text style={styles.text}>{image.width} x {image.height}</Text>
+                        <Text style={styles.text}>Size: {image.filesize}</Text>
+                        <Text style={styles.text}>{image.createdAt}</Text>
+                    </View>
+                )}
+            </ImageBackground>
+        </Pressable>
+    );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    grid: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        padding: 4,
-        backgroundColor: '#aeaeae',
-    },
     photo: {
-        height: '100%',
         width: '100%',
-        padding: 4,
+        justifyContent: 'flex-end',
+    },
+    textOverlay: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: 'flex-end',
+        padding: 16,
+    },
+    text: {
+        color: 'white',
+        fontSize: 10,
+        marginBottom: 8,
+    },
+    blackout: {
+        padding: 8,
+        backgroundColor: 'rgba(0,0,0,0.5)',
     },
 });
