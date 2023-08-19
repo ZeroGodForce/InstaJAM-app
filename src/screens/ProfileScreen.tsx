ceebc84be1e1242ef1dbc8f980ecb529e3b7a4a1
Captain Buckman <ZeroGodForce@users.noreply.github.com>
2023-08-19 18:31:36 +0100

    [PROFILE] Restyled button

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,27 +1,49 @@
 import React from 'react'
-import { StyleSheet } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useAuthApi } from '@/hooks';
-import { Button } from 'react-native-paper';
 
 export const ProfileScreen = ({ navigation }) => {
     const { deleteLogout } = useAuthApi();
+
     const handleLogOut = async () => {
         await deleteLogout()
     }
 
     return (
-        <SafeAreaView style={styles.container}>
-            <Button mode="outlined" onPress={handleLogOut}>Log out</Button>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+            <View style={styles.container}>
+                <Pressable onPress={handleLogOut}>
+                    <View style={styles.btnXL}>
+                        <Text style={styles.btnXLText}>Log out</Text>
+                    </View>
+                </Pressable>
+            </View>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
+        padding: 24,
+        flexGrow: 1,
+        flexShrink: 1,
+        flexBasis: 0,
         justifyContent: 'center',
+    },
+    btnXL: {
+        alignItems: 'center',
+        borderRadius: 8,
+        paddingVertical: 16,
+        paddingHorizontal: 24,
+        borderWidth: 1,
+        backgroundColor: '#f75249',
+        borderColor: '#f75249',
+    },
+    btnXLText: {
+        fontSize: 20,
+        lineHeight: 28,
+        fontWeight: '600',
+        color: '#fff',
     },
 });
